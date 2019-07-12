0253d9f351c584478c2de5a58d19fb4b02c62849
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-07-12 09:48:11 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
   end
 
   def show
-  @post = Post.find(params[:id])
-  @author = @post.author
-end
+    @post = Post.find(params[:id])
+    @author = @post.author
+  end
 
   def new
     @post = Post.new
